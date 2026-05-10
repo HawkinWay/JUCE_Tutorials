e9a3ec19d1fc47fc0228445b247e81acc94f847d
HurricaneWay <1826784860@qq.com>
2026-05-10 12:47:32 +0800

    version2.0, add Noise Level Slider

--- SimpleNoise/include/SimpleNoise.h
+++ SimpleNoise/include/SimpleNoise.h
@@ -5,6 +5,13 @@
 class SimpleNoise : public juce::AudioAppComponent {
 public:
     SimpleNoise() {
+        _levelSlider.setRange(0.0, 0.25);
+        _levelSlider.setTextBoxStyle(juce::Slider::/*TextEntryBoxPosition::*/TextBoxRight, false, 100, 20);
+        _levelLabel.setText("Noise Level", juce::/*NotificationType::*/dontSendNotification);
+
+        addAndMakeVisible(_levelSlider);
+        addAndMakeVisible(_levelLabel);
+
         setSize(800, 600);
         setAudioChannels(0,2);
     }
@@ -26,17 +33,31 @@ public:
     }
 
     void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override {
+
+        auto level = static_cast<float>(_levelSlider.getValue());
+
         for (auto channel = 0; channel < bufferToFill.buffer->getNumChannels(); channel++) {
+
             auto *buffer = bufferToFill.buffer->getWritePointer(channel, bufferToFill.startSample);
+
             for (auto sample = 0; sample < bufferToFill.buffer->getNumSamples(); sample++) {
-                buffer[sample] = _random.nextFloat() * 0.25f - 0.125f;
+
+                //buffer[sample] = _random.nextFloat() * 0.25f - 0.125f;
+                *buffer++ = _random.nextFloat() * level;
+
             }
         }
     }
 
+    void resized() override {
+        _levelSlider.setBounds(100, 10, getWidth() - 110, 20);
+        _levelLabel.setBounds(10, 10, 90, 20);
+    }
 
 private:
     juce::Random _random;
+    juce::Slider _levelSlider;
+    juce::Label _levelLabel;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleNoise)
 };
 
