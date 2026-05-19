a60ba0a20c4da9033d2b52b973baaffa457b0e29
HawkinWay <1826784860@qq.com>
2026-05-19 17:08:04 +0800

    add opentButtonClicked() AudioPlayer.h

--- AudioPlayer/include/AudioPlayer.h
+++ AudioPlayer/include/AudioPlayer.h
@@ -46,11 +46,15 @@ public:
 
 
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override{
-
+        transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
     }
 
     void getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill) override{
-
+        if(readerSource.get() == nullptr){
+            bufferToFill.clearActiveBufferRegion();
+            return;
+        }
+        transportSource.getNextAudioBlock(bufferToFill);
     }
 
     void releaseResources() override{
@@ -63,19 +67,59 @@ public:
 
 private:
     void changeState(TransportState newState){
-
+        if(state != newState){
+            state = newState;
+            switch(state){
+                case TransportState::Stopped:
+                    stopButton.setEnabled(false);
+                    playButton.setEnabled(true);
+                    transportSource.setPosition(0.0);
+                    break;
+                case TransportState::Starting:
+                    playButton.setEnabled(false);
+                    transportSource.start();
+                    break;
+                case TransportState::Playing:
+                    stopButton.setEnabled(true);
+                    break;
+                case TransportState::Stopping:
+                    transportSource.stop();
+                    break;
+            }
+        }
     }
 
     void openButtonClicked(){
+        chooser = std::make_unique<juce::FileChooser>(
+                    "Select a file(wav, mp3) to play...",
+                    juce::File {},
+                    "*.wave;*.mp3"
+                );
 
+        auto chooserFlags = juce::FileBrowserComponent::openMode
+                          | juce::FileBrowserComponent::canSelectFiles;
+
+        chooser->launchAsync(chooserFlags,
+                             [this](const juce::FileChooser& fc){
+                                auto file = fc.getResult();
+                                if(file != juce::File {}){
+                                    auto* reader = formatManager.createReaderFor(file);
+                                    if(reader != nullptr){
+                                        auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                                        transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+                                        playButton.setEnabled(true);
+                                        readerSource.reset(newSource.release());
+                                    }
+                                }
+                             });
     }
 
     void playButtonClicked(){
-
+        changeState(TransportState::Starting);
     }
 
     void stopButtonClicked(){
-
+        changeState(TransportState::Stopping);
     }
 
     juce::TextButton openButton;
@@ -83,8 +127,9 @@ private:
     juce::TextButton stopButton;
 
     TransportState state;
+    std::unique_ptr<juce::FileChooser> chooser;
 
     juce::AudioFormatManager formatManager;
-    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;    // have benefit of being exception-safe when AudioTransportSource::setSource()
     juce::AudioTransportSource transportSource;
 };
